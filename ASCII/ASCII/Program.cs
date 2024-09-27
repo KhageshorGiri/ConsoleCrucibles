9064f4ba46937addb58e111d7a743c918b39d126
KhageshorGiri <girikhageshor2432@gmail.com>
2024-09-27 20:08:50 +0545

    Learning ascii

--- ASCII/ASCII/Program.cs
+++ ASCII/ASCII/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+
+using ASCII;
+using static System.Console;
+
+
+const int window_width = 120;
+const int window_height = 60;
+
+//WindowWidth = window_width;
+//WindowHeight = window_height;
+
+AscII ascII = new();
+ascII.Display();
