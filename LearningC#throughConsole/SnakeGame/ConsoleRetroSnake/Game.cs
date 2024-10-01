ffdac09ee809859c1fdc62c9b31bd5f60815cb0d
KhageshorGiri <girikhageshor2432@gmail.com>
2024-10-01 22:44:47 +0545

    Added Function to handel user input

--- LearningC#throughConsole/SnakeGame/ConsoleRetroSnake/Game.cs
+++ LearningC#throughConsole/SnakeGame/ConsoleRetroSnake/Game.cs
@@ -5,8 +5,15 @@ namespace ConsoleRetroSnake;
 public class Game
 {
 
+    WindowObject grid ;
+    WindowObject snake ;
+    WindowObject food ;
+
     public Game()
     {
+         grid = new WindowObject(60, 20);
+         snake = new WindowObject(40, 10);
+         food = new WindowObject(20, 5);
 
     }
 
@@ -14,19 +21,26 @@ public class Game
     {
         DisplayIntro();
 
-        DisplayWindow();
 
-        WaitForKeyPress();
+        while (true)
+        {
+
+            DisplayWindow();
+
+            HandelPlayerInput();
+
+            Thread.Sleep(20);
+        }
+
+        DisplayOutro();
+
     }
 
 
     public void DisplayWindow()
     {
-        //Clear();
-        var grid = new WindowObject(60, 20);
-        var snake = new WindowObject(40, 10);
-        var food = new WindowObject(20, 5);
-       
+        Clear();
+
         // here y : cols and x : rows
         for(int y = 0; y< grid.Y; y++)
         {
@@ -58,17 +72,44 @@ public class Game
             WriteLine();
         }
 
+        SetCursorPosition(snake.X+1, snake.Y);
+    }
+
+    public void HandelPlayerInput()
+    {
+        ConsoleKeyInfo keyInfo = Console.ReadKey();
+        ConsoleKey key = keyInfo.Key;
+
+        switch (key)
+        {
+            case ConsoleKey.UpArrow:
+                break;
+            case ConsoleKey.DownArrow:
+                break;
+            case ConsoleKey.LeftArrow:
+                break;
+            case ConsoleKey.RightArrow:
+                break;
+            default:
+                break;
+        }
     }
 
     public void DisplayIntro()
     {
         WriteLine("!!! Welcome TO Console Snake Game !!!");
         WriteLine("=========================================");
+        WriteLine("\nPress any key to start the game");
+        ReadKey(true);
     }
 
-    public void WaitForKeyPress()
+
+    public void DisplayOutro()
     {
+        Clear();
+        WriteLine("Thanks for playing game.");
         WriteLine("\nPress any key to exist...");
         ReadKey(true);
     }
+  
 }
