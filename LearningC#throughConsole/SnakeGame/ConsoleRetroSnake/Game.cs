7d981a7de64c5a2cb79a578fecafcdef1d3aa7a2
KhageshorGiri <girikhageshor2432@gmail.com>
2024-10-02 18:49:56 +0545

    Added Code to handel to detect wall collision and food colision

--- LearningC#throughConsole/SnakeGame/ConsoleRetroSnake/Game.cs
+++ LearningC#throughConsole/SnakeGame/ConsoleRetroSnake/Game.cs
@@ -8,11 +8,12 @@ public class Game
     WindowObject grid ;
     WindowObject snake ;
     WindowObject food ;
+    bool play = true;
 
     public Game()
     {
-         grid = new WindowObject(60, 20);
-         snake = new WindowObject(40, 10);
+         grid = new WindowObject(30, 20);
+         snake = new WindowObject(10, 10);
          food = new WindowObject(20, 5);
 
     }
@@ -21,12 +22,15 @@ public class Game
     {
         DisplayIntro();
 
-
-        while (true)
+        while (play)
         {
 
             DisplayWindow();
 
+            //if (CollisonDetectionWithWall(snake.X, snake.Y))
+            //    AskUserToPlayAgain();
+
+            CollisonDetectionWithFood(snake.X, snake.Y);
             HandelPlayerInput();
 
             Thread.Sleep(20);
@@ -36,6 +40,14 @@ public class Game
 
     }
 
+    public void AskUserToPlayAgain()
+    {
+        Write("\nDo you want to play again?[Y/N]: ");
+        var userChoice = ReadLine()?.Trim().ToLower();
+        if(userChoice == "yes" || userChoice == "y")
+            play = true;
+        play = false;
+    }
 
     public void DisplayWindow()
     {
@@ -75,6 +87,18 @@ public class Game
         SetCursorPosition(snake.X+1, snake.Y);
     }
 
+    public bool CollisonDetectionWithWall(int x , int y)
+    {
+        if(x == 15 || y == 15)
+            return true;
+        return false;
+    }
+
+    public void CollisonDetectionWithFood(int x, int y)
+    {
+        if (x == food.X && y == food.Y)
+            WriteLine("Food Found");
+    }
     public void HandelPlayerInput()
     {
         ConsoleKeyInfo keyInfo = Console.ReadKey();
@@ -83,16 +107,37 @@ public class Game
         switch (key)
         {
             case ConsoleKey.UpArrow:
+                if(IsLocationMoveable(snake.X, snake.Y))
+                    snake.UpdateYCoordinate(snake.Y - 1);
+                   
                 break;
+
             case ConsoleKey.DownArrow:
+                if (IsLocationMoveable(snake.X, snake.Y+1))
+                    snake.UpdateYCoordinate(snake.Y + 1);
+               
                 break;
+
             case ConsoleKey.LeftArrow:
+                if (IsLocationMoveable(snake.X, snake.Y))
+                    snake.UpdateXCooridnate(snake.X - 1);
                 break;
+
             case ConsoleKey.RightArrow:
+                if (IsLocationMoveable(snake.X+1, snake.Y))
+                    snake.UpdateXCooridnate(snake.X + 1);
                 break;
+
             default:
                 break;
         }
+    }
+
+    public bool IsLocationMoveable(int x, int y)
+    {
+        if(x > 0 && y > 0 && x < grid.X && y < grid.Y)
+            return true;
+        return false;
     }
 
     public void DisplayIntro()
--- LearningC#throughConsole/SnakeGame/ConsoleRetroSnake/WindowObject.cs
+++ LearningC#throughConsole/SnakeGame/ConsoleRetroSnake/WindowObject.cs
@@ -15,4 +15,20 @@ public class WindowObject
         y = yaxis;
     }
 
+    public void UpdateXCooridnate(int x_new)
+    {
+        x = x_new;
+    }
+
+    public void UpdateYCoordinate(int y_new)
+    {
+        y = y_new; 
+    }
+
+    public void UpdateCoordinate(int x_new , int y_new)
+    {
+        x = x_new;
+        y = y_new;
+    }
+   
 }
