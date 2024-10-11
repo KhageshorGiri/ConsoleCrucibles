c660f18c179e8563aabb518c08ed4f3decb773bd
KhageshorGiri <girikhageshor2432@gmail.com>
2024-10-11 20:48:54 +0545

    Added Project setup and design

---
+++ Pong Game/Pong Game/PongGameForm.Designer.cs
@@ -0,0 +1,95 @@
+﻿namespace Pong_Game
+{
+    partial class PongGameForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            pictureBox1 = new PictureBox();
+            computer = new PictureBox();
+            ball = new PictureBox();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)computer).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)ball).BeginInit();
+            SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Image = Properties.Resources.player;
+            pictureBox1.Location = new Point(2, 137);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(30, 120);
+            pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox1.TabIndex = 0;
+            pictureBox1.TabStop = false;
+            pictureBox1.Click += pictureBox1_Click;
+            // 
+            // computer
+            // 
+            computer.Image = Properties.Resources.computer;
+            computer.Location = new Point(770, 137);
+            computer.Name = "computer";
+            computer.Size = new Size(30, 120);
+            computer.SizeMode = PictureBoxSizeMode.StretchImage;
+            computer.TabIndex = 1;
+            computer.TabStop = false;
+            computer.Click += pictureBox2_Click;
+            // 
+            // ball
+            // 
+            ball.Image = Properties.Resources.ball;
+            ball.Location = new Point(381, 171);
+            ball.Name = "ball";
+            ball.Size = new Size(30, 30);
+            ball.SizeMode = PictureBoxSizeMode.StretchImage;
+            ball.TabIndex = 2;
+            ball.TabStop = false;
+            ball.Click += pictureBox3_Click;
+            // 
+            // PongGameForm
+            // 
+            AutoScaleDimensions = new SizeF(8F, 19F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.Black;
+            ClientSize = new Size(800, 450);
+            Controls.Add(ball);
+            Controls.Add(computer);
+            Controls.Add(pictureBox1);
+            Name = "PongGameForm";
+            Text = "Player : 0 | Computer : 0";
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)computer).EndInit();
+            ((System.ComponentModel.ISupportInitialize)ball).EndInit();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private PictureBox pictureBox1;
+        private PictureBox computer;
+        private PictureBox ball;
+    }
+}
