ef0bd36c423700fce4c5e5ea78b1032e7730c61d
KhageshorGiri <girikhageshor2432@gmail.com>
2024-10-20 16:00:00 +0545

    IMplemented Snake properties

--- Snake--upgraded/SankeUpgraded/SankeUpgraded/Snake.cs
+++ Snake--upgraded/SankeUpgraded/SankeUpgraded/Snake.cs
@@ -1,9 +1,4 @@
 ﻿using SankeUpgraded.Enums;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace SankeUpgraded;
 
@@ -15,23 +10,62 @@ public class Snake
     private readonly ConsoleColor HeadColor;
     private readonly ConsoleColor BodyColor;
 
-    public Snake()
+    public Cell Head {  get; private set; }
+    public Queue<Cell> Body { get; } = new Queue<Cell>();
+  
+    public Snake(int initialX, int initialY, 
+        ConsoleColor headColor, ConsoleColor bodyColor, 
+        int bodyLenght = 3)
     {
+        HeadColor = headColor;
+        BodyColor = bodyColor;
 
+        Head = new Cell(initialX, initialY, headColor);
+
+        for( int i = bodyLenght; i > 0; i--)
+        {
+            Body.Enqueue(new Cell(Head.X - i - 1, initialY, bodyColor));
+        }
+
+        Draw();
     }
 
-    public void Move(Direction direction)
+    public void Move(Direction direction, bool eat = false)
     {
 
+        Clear();
+
+        Body.Enqueue(new Cell(Head.X, Head.Y, BodyColor));
+        if (!eat)
+            Body.Dequeue();
+
+        Head = direction switch
+        {
+            Direction.Right => new Cell(Head.X + 1, Head.Y, HeadColor),
+            Direction.Left => new Cell(Head.X - 1, Head.Y, HeadColor),
+            Direction.Up => new Cell(Head.X, Head.Y - 1, HeadColor),
+            Direction.Down => new Cell(Head.X, Head.Y + 1, HeadColor),
+            _ => Head
+        };
     }
 
     public void Draw()
     {
+        Head.Draw();
 
+        foreach (Cell cell in Body)
+        {
+            cell.Draw();
+        }
     }
 
     public void Clear()
     {
+        Head.Clear();
 
+        foreach(Cell cell in Body)
+        {
+            cell.Clear();
+        }
     }
 }
